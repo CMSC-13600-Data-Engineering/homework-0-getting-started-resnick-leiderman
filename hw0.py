c06a6caab8f7880fa02fe661c46aa81fea264adc
maxwell-resnick <128745062+maxwell-resnick@users.noreply.github.com>
2023-03-26 17:32:26 -0500

    finished hw0

--- hw0.py
+++ hw0.py
@@ -2,7 +2,6 @@
 Introductory programming assignment.
 """
 import pandas as pd
-import numpy as np
 
 """
 !!!TODO!!!
@@ -11,7 +10,15 @@ def read_file(filename):
 	'''Input: filename (location of stock.dat)
 	   Output: a list of tuples (ticker, name)
 	'''
-	return None
+	file = open(filename, "r")
+	lines = file.readlines()
+	tuple_list = []
+	for line in lines:
+		pairs = line.split(" - ")
+		pairs[1] = pairs[1].strip('\n')
+		tup = (pairs[0], pairs[1])
+		tuple_list.append(tup)
+	return tuple_list
 
 
 """
@@ -21,4 +28,7 @@ def write_csv(parsed, outfile):
 	'''Input: a list of tuples (ticker,name), output location
 	   Output: None/Void, writes a file
 	'''
-	return
+	df = pd.DataFrame(parsed, columns=['ticker','name'])
+	df.set_index('ticker', inplace=True)
+	df.to_csv(outfile, header=True)
+	return None
